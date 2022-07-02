4ae80c7b04885afd4fa42f40cf93c83167b0dc98
Andrey <dev.andr@yandex.com>
2022-07-02 18:56:43 +0500

    feat: add ItemRequest fields

--- src/main/java/ru/practicum/shareit/requests/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequest.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.requests;
 
-/**
- * // TODO .
- */
+import lombok.*;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor
 public class ItemRequest {
+
+    private Long id;
+
+    @NotEmpty(message = "Description should not be empty")
+    @Size(max = 200, message = "Description should less 200 characters")
+    private String description;
+
+    @NotNull(message = "User should not be null")
+    private User requestor;
+
+    @NotNull(message = "Created date should not be null")
+    private LocalDateTime created;
 }

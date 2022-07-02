faaddb4604a72d39a95c28ba321295eb2a6e73cb
Andrey <dev.andr@yandex.com>
2022-07-02 18:33:53 +0500

    feat: add User fields

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.user;
 
-/**
- * // TODO .
- */
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor
 public class User {
+
+    private Long id;
+
+    @NotEmpty(message = "Name should not be empty")
+    private String name;
+
+    @Email(message = "Email should be valid")
+    private String email;
 }

c6d4c6903cb0873a6f45bc69c5ca2e1ebe59add0
Andrey <dev.andr@yandex.com>
2022-07-02 18:05:56 +0500

    feat: add Item fields

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,34 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * // TODO .
- */
+import lombok.*;
+import ru.practicum.shareit.requests.ItemRequest;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor
 public class Item {
+
+    private Long id;
+
+    @NotEmpty(message = "Name should not be empty")
+    private String name;
+
+    @NotEmpty(message = "Description should not be empty")
+    @Size(max = 200, message = "Description should less 200 characters")
+    private String description;
+
+    @NotEmpty(message = "Status should not be empty")
+    private Boolean available;
+
+    @NotNull(message = "User should not be null")
+    private User owner;
+
+    private ItemRequest request;
 }

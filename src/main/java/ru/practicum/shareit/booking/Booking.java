b6772758fa56360107cd153a39c8000a989e3570
Andrey <dev.andr@yandex.com>
2022-07-02 18:43:28 +0500

    feat: add Booking fields

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,33 @@
 package ru.practicum.shareit.booking;
 
-/**
- * // TODO .
- */
+import lombok.*;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@RequiredArgsConstructor
 public class Booking {
+
+    private Long id;
+
+    @NotNull(message = "Start date should not be null")
+    private LocalDate start;
+
+    @NotNull(message = "End date should not be null")
+    private LocalDate end;
+
+    @NotNull(message = "Item should not be null")
+    private Item item;
+
+    @NotNull(message = "Booker should not be null")
+    private User booker;
+
+    @NotNull(message = "Status should not be null")
+    private Status status;
 }

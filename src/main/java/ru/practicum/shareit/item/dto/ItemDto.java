b995acd8c00573f0cca15d208b0c7c0e21a41ae5
Andrey <dev.andr@yandex.com>
2022-07-04 20:57:53 +0500

    fix: ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -2,20 +2,35 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import ru.practicum.shareit.requests.dto.ItemRequestDto;
-import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Setter
 @Getter
 public class ItemDto {
 
     private Long id;
-    private String name;
-    private String description;
-    private boolean available;
-    private User owner;
-    private ItemRequestDto request;
 
-    public ItemDto(String name, String description, boolean available, Long requestId) {
+    @NotEmpty(message = "Name should not be empty")
+    private String name;
+
+    @NotEmpty(message = "Description should not be empty")
+    private String description;
+
+    @NotNull(message = "Available should not be empty")
+    private Boolean available;
+    private Long requestId;
+
+    public ItemDto(Long id,
+                   String name,
+                   String description,
+                   Boolean available,
+                   Long requestId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.available = available;
+        this.requestId = requestId;
     }
 }

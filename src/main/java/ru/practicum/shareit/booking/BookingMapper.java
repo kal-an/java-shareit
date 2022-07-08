78132b3c48d0939f13ad4bde1bfbbc489ca65f40
Andrey <dev.andr@yandex.com>
2022-07-08 20:57:55 +0500

    fix: add field item name in method BookingMapper

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -9,6 +9,7 @@ public class BookingMapper {
         return new BookingDto(
                 booking.getStart(),
                 booking.getEnd(),
+                booking.getItem().getName(),
                 booking.getBooker().getId(),
                 booking.getStatus()
         );

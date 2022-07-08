25a8606bd708cb8fd6126d3b87614d2a0d8dc8ff
Andrey <dev.andr@yandex.com>
2022-07-08 21:26:33 +0500

    fix: delete unused imports in BookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import lombok.Getter;
 import lombok.Setter;
 import ru.practicum.shareit.booking.Status;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDate;
 

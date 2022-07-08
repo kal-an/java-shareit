ffd1d7ecfa156ce28cf9eb4070f0c6a41f3d2399
Andrey <dev.andr@yandex.com>
2022-07-08 21:08:40 +0500

    feat: add ItemMapper

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -7,10 +7,19 @@ public class ItemMapper {
 
     public static ItemDto toItemDto(Item item) {
         return new ItemDto(
+                item.getId(),
                 item.getName(),
                 item.getDescription(),
-                item.isAvailable(),
+                item.getAvailable(),
                 item.getRequest() != null ? item.getRequest().getId() : null
         );
+    }
+
+    public static Item toItem(ItemDto itemDto) {
+        final Item item = new Item();
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        return item;
     }
 }

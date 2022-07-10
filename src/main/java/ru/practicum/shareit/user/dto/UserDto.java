d52c1e354e101e9e41beea5d0b41c3cba7f042d2
Andrey <dev.andr@yandex.com>
2022-07-10 22:39:30 +0500

    fix: UserDto

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,20 +1,27 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @Setter
 @Getter
-@AllArgsConstructor
 public class UserDto {
 
     private Long id;
+
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email should not be empty")
     private String email;
 
-    public UserDto(Long id, String email) {
+    public UserDto(Long id, String name, String email) {
         this.id = id;
+        this.name = name;
         this.email = email;
     }
 }

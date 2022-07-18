07146badd13d9660cdf5a16a098169632f8d0217
Andrey <dev.andr@yandex.com>
2022-07-18 16:28:20 +0500

    feat: add JPA annotation to Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.model;
 import lombok.*;
 import ru.practicum.shareit.requests.model.ItemRequest;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -12,22 +13,30 @@ import javax.validation.constraints.Size;
 @EqualsAndHashCode
 @ToString
 @RequiredArgsConstructor
+@Entity
+@Table(name = "items", schema = "public")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty(message = "Name should not be empty")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @NotEmpty(message = "Description should not be empty")
     @Size(max = 200, message = "Description should less 200 characters")
+    @Column(name = "description", nullable = false, length = 4000)
     private String description;
 
     @NotEmpty(message = "Status should not be empty")
     private Boolean available;
 
     @NotNull(message = "User should not be null")
+    @Column(name = "owner_id", nullable = false)
     private Long owner;
 
+    @Column(name = "request_id")
     private ItemRequest request;
 }

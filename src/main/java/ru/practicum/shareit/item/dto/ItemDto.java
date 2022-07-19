c7c67274de7789871c369300c0bd771dcec67c16
Andrey <dev.andr@yandex.com>
2022-07-19 17:50:16 +0500

    feat: add extra annotations to Item DTO

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Setter
 @Getter
@@ -13,9 +14,11 @@ public class ItemDto {
     private Long id;
 
     @NotEmpty(message = "Name should not be empty")
+    @Size(max = 255, message = "Name should less 255 characters")
     private String name;
 
     @NotEmpty(message = "Description should not be empty")
+    @Size(max = 4000, message = "Description should less 4000 characters")
     private String description;
 
     @NotNull(message = "Available should not be empty")

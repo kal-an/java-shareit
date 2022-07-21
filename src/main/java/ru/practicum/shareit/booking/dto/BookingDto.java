4a7892e231a2ce88d44cccefee0c09ba989199f7
Andrey <dev.andr@yandex.com>
2022-07-21 11:02:10 +0500

    fix: change fields in Booking DTO

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,34 +2,38 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import ru.practicum.shareit.booking.model.Status;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
 
-import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
 public class BookingDto {
 
     private Long id;
-    private LocalDate start;
-    private LocalDate end;
-    private Item item;
-    private Long userId;
-    private String status;
 
-    public BookingDto(LocalDate start,
-                      LocalDate end,
-                      String name,
-                      Long userId,
-                      String status) {
+    @NotNull(message = "Start date should not be null")
+    private LocalDateTime start;
+
+    @NotNull(message = "End date should not be null")
+    private LocalDateTime end;
+
+    private ItemDto item;
+
+    private UserDto booker;
+
+    private Status status;
+
+    public BookingDto(Long id,
+                      LocalDateTime start,
+                      LocalDateTime end,
+                      Status status) {
+        this.id = id;
         this.start = start;
         this.end = end;
-        this.item.name = name;
-        this.userId = userId;
         this.status = status;
-    }
-
-    private static class Item {
-        private long id;
-        private String name;
     }
 }

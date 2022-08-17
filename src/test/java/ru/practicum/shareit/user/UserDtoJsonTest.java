1180c3ecead88a7ecb7073d2f50c8215d82eee26
Andrey <dev.andr@yandex.com>
2022-08-17 17:39:59 +0500

    fix: remove unused imports

--- src/test/java/ru/practicum/shareit/user/UserDtoJsonTest.java
+++ src/test/java/ru/practicum/shareit/user/UserDtoJsonTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
